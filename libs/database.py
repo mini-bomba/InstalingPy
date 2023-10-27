f2899d4cb5c4abdc6ce646fff737c4ae5d37eb8d
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2023-10-27 22:04:09 +0200

    Fix typing of aiomysql in database.py
    This was done by creating unused subclasses with proper typing

--- libs/database.py
+++ libs/database.py
@@ -1,13 +1,36 @@
+import contextlib
 import datetime
-import itertools
+import typing
+from abc import ABC
 
 import aiomysql
+
 from . import classes
 from . import utils
 
+T = typing.TypeVar("T", aiomysql.Cursor, aiomysql.DictCursor, aiomysql.SSCursor, aiomysql.SSDictCursor)
+
+
+class PatchedConnection(aiomysql.Connection):
+    @typing.overload
+    def cursor(self) -> contextlib.AbstractAsyncContextManager[aiomysql.Cursor]:
+        pass
+
+    @typing.overload
+    def cursor(self, cur_type: type[T]) -> contextlib.AbstractAsyncContextManager[T]:
+        pass
+
+    def cursor(self, *cur_type):
+        return super().cursor(*cur_type)
+
+
+class PatchedPool(aiomysql.Pool, ABC):
+    def acquire(self) -> contextlib.AbstractAsyncContextManager[PatchedConnection]:
+        return super().acquire()
+
 
 class DatabaseManager:
-    pool: aiomysql.Pool
+    pool: PatchedPool
     args: tuple
     kwargs: dict
 
@@ -31,37 +54,29 @@ class DatabaseManager:
         await self.close()
 
     async def word_exists(self, word_id: int) -> bool:
-        async with self.pool.acquire() as connection:
-            async with connection.cursor(aiomysql.DictCursor) as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.DictCursor
+        async with self.pool.acquire() as connection, connection.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute("""
+                SELECT id FROM Words
+                WHERE id = %s
+                LIMIT 1
+            """, (word_id,))
+            return await cursor.fetchone() is not None
 
+    async def get_word(self, word_id: int, user_id: int | None = None) -> classes.DBWord | None:
+        async with self.pool.acquire() as connection, connection.cursor(aiomysql.DictCursor) as cursor:
+            if user_id is None:
                 await cursor.execute("""
-                    SELECT id FROM Words
+                    SELECT * FROM Words
                     WHERE id = %s
                     LIMIT 1
                 """, (word_id,))
-                return await cursor.fetchone() is not None
-
-    async def get_word(self, word_id: int, user_id: int | None = None) -> classes.DBWord | None:
-        async with self.pool.acquire() as connection:
-            async with connection.cursor(aiomysql.DictCursor) as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.DictCursor
-
-                if user_id is None:
-                    await cursor.execute("""
-                        SELECT * FROM Words
-                        WHERE id = %s
-                        LIMIT 1
-                    """, (word_id,))
-                else:
-                    await cursor.execute("""
-                        SELECT Words.*, WordHistory.seen_times, WordHistory.last_seen
-                        FROM WordHistory INNER JOIN Words ON WordHistory.word_id = Words.id
-                        WHERE WordHistory.word_id = %s AND WordHistory.user_id = %s;
-                    """, (word_id, user_id))
-                result = await cursor.fetchone()
+            else:
+                await cursor.execute("""
+                    SELECT Words.*, WordHistory.seen_times, WordHistory.last_seen
+                    FROM WordHistory INNER JOIN Words ON WordHistory.word_id = Words.id
+                    WHERE WordHistory.word_id = %s AND WordHistory.user_id = %s;
+                """, (word_id, user_id))
+            result = await cursor.fetchone()
         if result is None:
             return None
         if user_id is None:
@@ -70,35 +85,25 @@ class DatabaseManager:
         return classes.DBWord(**result, translations=translations)
 
     async def get_word_translations(self, word_id: int) -> list[str]:
-        async with self.pool.acquire() as connection:
-            async with connection.cursor() as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.Cursor
-
-                await cursor.execute("""
-                    SELECT translation FROM WordTranslations
-                    WHERE word_id = %s
-                """, (word_id,))
-                return [t[0] for t in await cursor.fetchall()]
+        async with self.pool.acquire() as connection, connection.cursor() as cursor:
+            await cursor.execute("""
+                SELECT translation FROM WordTranslations
+                WHERE word_id = %s
+            """, (word_id,))
+            return [t[0] for t in await cursor.fetchall()]
 
     async def get_seen_data(self, word_id: int, user_id: int) -> tuple[int, datetime.datetime]:
-        async with self.pool.acquire() as connection:
-            async with connection.cursor() as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.Cursor
-
-                await cursor.execute("""
-                    SELECT seen_times, last_seen FROM WordHistory
-                    WHERE word_id = %s AND user_id = %s
-                    LIMIT 1
-                """, (word_id, user_id))
-                return await cursor.fetchone()
+        async with self.pool.acquire() as connection, connection.cursor() as cursor:
+            await cursor.execute("""
+                SELECT seen_times, last_seen FROM WordHistory
+                WHERE word_id = %s AND user_id = %s
+                LIMIT 1
+            """, (word_id, user_id))
+            return await cursor.fetchone()
 
     async def translate_words(self, translations: list[str], user_id: int | None = None) -> list[classes.DBWord]:
         async with self.pool.acquire() as connection:
             async with connection.cursor(aiomysql.DictCursor) as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.DictCursor
 
                 if user_id is None:
                     await cursor.execute("""
@@ -116,7 +121,6 @@ class DatabaseManager:
                     """, (translations, user_id))
                 result1 = await cursor.fetchall()
             async with connection.cursor() as cursor:
-                cursor: aiomysql.Cursor
                 words = []
                 for word in result1:
                     await cursor.execute("""
@@ -134,42 +138,35 @@ class DatabaseManager:
         return words
 
     async def insert_word(self, word: classes.WordData, user_id: int | None = None):
-        async with self.pool.acquire() as connection:
-            async with connection.cursor() as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.Cursor
-
-                # word
-                await cursor.execute("""
-                    INSERT INTO Words (id, word, shown_word, usage_example)
-                    VALUES (%s, %s, %s, %s);
-                """, (word.id, word.word, word.shown_answer, word.usage_example))
-                # translations
-                translations = utils.split_translations(word.translations)
+        async with self.pool.acquire() as connection, connection.cursor() as cursor:
+            # word
+            await cursor.execute("""
+                INSERT INTO Words (id, word, shown_word, usage_example)
+                VALUES (%s, %s, %s, %s);
+            """, (word.id, word.word, word.shown_answer, word.usage_example))
+            # translations
+            translations = utils.split_translations(word.translations)
+            await cursor.execute(f"""
+                INSERT INTO WordTranslations (word_id, translation)
+                VALUES {', '.join(('(%s, %s)',) * len(translations))};
+            """, [i for t in translations for i in (word.id, t)])
+            # seen data
+            if user_id is not None:
                 await cursor.execute(f"""
-                    INSERT INTO WordTranslations (word_id, translation)
-                    VALUES {', '.join(('(%s, %s)',) * len(translations))};
-                """, [i for t in translations for i in (word.id, t)])
-                # seen data
-                if user_id is not None:
-                    await cursor.execute(f"""
-                        INSERT INTO WordHistory (word_id, user_id) 
-                        VALUES (%s, %s);
-                    """, (word.id, user_id))
+                    INSERT INTO WordHistory (word_id, user_id) 
+                    VALUES (%s, %s);
+                """, (word.id, user_id))
 
-                await connection.commit()
+            await connection.commit()
 
     async def mark_word_as_seen(self, word_id: int, user_id: int):
-        async with self.pool.acquire() as connection:
-            async with connection.cursor() as cursor:
-                connection: aiomysql.Connection
-                cursor: aiomysql.Cursor
-                await cursor.execute(f"""
-                    INSERT INTO WordHistory (word_id, user_id)
-                    VALUES (%s, %s)
-                    ON DUPLICATE KEY UPDATE seen_times = seen_times + 1;
-                """, (word_id, user_id))
-                await connection.commit()
+        async with self.pool.acquire() as connection, connection.cursor() as cursor:
+            await cursor.execute(f"""
+                INSERT INTO WordHistory (word_id, user_id)
+                VALUES (%s, %s)
+                ON DUPLICATE KEY UPDATE seen_times = seen_times + 1;
+            """, (word_id, user_id))
+            await connection.commit()
 
     async def handle_word(self, word: classes.WordData, user_id: int):
         if await self.word_exists(word.id):
